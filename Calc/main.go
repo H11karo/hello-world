05997f28861120fbd62b39079575502307742048
H1karo <hikaroplays@gmail.com>
2024-05-09 01:35:53 +0200

    Update main.go
    shortened variables

--- Calc/main.go
+++ Calc/main.go
@@ -6,35 +6,35 @@ import (
 
 func main() {
 
-	var number1, number2 int64
-	var operator string
+	var n1, n2 int64
+	var o string
 
 	fmt.Println("Hello, this is a calculator")
 	fmt.Println("First Number:")
-	fmt.Scanln(&number1)
+	fmt.Scanln(&n1)
 
 	fmt.Println("+,-,*,/")
-	fmt.Scanln(&operator)
+	fmt.Scanln(&o)
 
 	fmt.Println("Second Number:")
-	fmt.Scanln(&number2)
+	fmt.Scanln(&n2)
 
-	switch operator {
+	switch o {
 
 	case "+":
-		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1+number2)
+		fmt.Printf("%d %s %d = %d", n1, or, n2, n1+n2)
 
 	case "-":
-		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1-number2)
+		fmt.Printf("%d %s %d = %d", n1, o, n2, n1-n2)
 
 	case "*":
-		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1*number2)
+		fmt.Printf("%d %s %d = %d", n1, o, n2, n1*n2)
 
 	case "/":
-		if number2 == 0 {
+		if n2 == 0 {
 			fmt.Println("That's not possible, dumbass")
 		} else {
-			fmt.Printf("%d %s %d = %d", number1, operator, number2, number1/number2)
+			fmt.Printf("%d %s %d = %d", n1, o, n2, n1/n2)
 		}
 	default:
 		fmt.Println("Please choose a valid operator")
